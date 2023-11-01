45c7df00610360fabd9b31956497f505a64f6f1d
hw7wh <hamzawahbi.pro@gmail.com>
2023-11-01 09:24:57 +0100

    Initial commit

---
+++ src/main/java/emsi/ace/tprest/TprestApplication.java
@@ -0,0 +1,13 @@
+package emsi.ace.tprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TprestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emsi/ace/tprest/TprestApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.ace.tprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TprestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
